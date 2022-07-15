5166474da612794d8ba2ff5cc63e63a9b104f2db
somang-lim <somangjuha@gmail.com>
2022-07-15 14:48:41 +0900

    등록할 때마다 생성되는 명언번호 증가

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -5,13 +5,11 @@ import java.util.Scanner;
 public class App {
 
     private static Scanner sc;
-
-    public App() {
-        sc = new Scanner(System.in);
-    }
+    private static int wiseSayingLastId;
 
     public App(Scanner sc) {
         this.sc = sc;
+        wiseSayingLastId = 0;
     }
 
     public static void run() {
@@ -24,11 +22,12 @@ public class App {
 
             switch(cmd) {
                 case "등록" :
+                    wiseSayingLastId++;
                     System.out.print("명언 : ");
                     String content = sc.nextLine();
                     System.out.print("작가 : ");
                     String author = sc.nextLine();
-                    System.out.printf("%d번 명언이 등록되었습니다.\n", 1);
+                    System.out.printf("%d번 명언이 등록되었습니다.\n", wiseSayingLastId);
                     break;
                 case "종료" :
                     break outer;
