926254b6beb512c40eca773e076c9b06f8153fc8
somang-lim <somangjuha@gmail.com>
2022-07-15 16:24:45 +0900

    존재하지 않는 명언삭제에 대한 예외처리

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -56,6 +56,10 @@ public class App {
 
                     WiseSaying wiseSaying = findById(id);
 
+                    if(wiseSaying == null) {
+                        System.out.printf("%d번 명언이 존재하지 않습니다.", id);
+                    }
+
                     wiseSayings.remove(wiseSaying);
 
                     System.out.printf("%d번 명언이 삭제되었습니다.\n", id);
